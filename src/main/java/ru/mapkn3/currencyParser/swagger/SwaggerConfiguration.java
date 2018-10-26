7544eb6afe23ce24a19ce13c18b1c2ead9a1febe
Mapkn3 <79608236848@ya.ru>
2018-10-26 16:08:59 +0400

    Add ApiInfo.

--- src/main/java/ru/mapkn3/currencyParser/swagger/SwaggerConfiguration.java
+++ src/main/java/ru/mapkn3/currencyParser/swagger/SwaggerConfiguration.java
@@ -3,21 +3,45 @@ package ru.mapkn3.currencyParser.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration {
+public class SwaggerConfiguration extends WebMvcConfigurationSupport {
     @Bean
     public Docket documentation() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Currency parser API")
+                .description("Currency parser API for parse currency rates for available in database banks")
+                .version("1.0.0")
+                .contact(new Contact("Arthur Kogan", "https://github.com/Mapkn3/", "79608236848@ya.ru"))
                 .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
